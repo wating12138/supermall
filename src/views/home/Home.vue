bec0c17aa0e6ffeebc78b59abb95b7da4d4f2c37
wating12138 <42110911+wating12138@users.noreply.github.com>
2019-12-02 18:39:55 +0800

    弹性布局和tabcontrol

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,21 +5,77 @@
     </nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <tab-control :text="['流行','新款','精选']" class="tab-control"/>
+    <p>1</p>
+    <p>2</p>
+    <p>3</p>
+    <p>4</p>
+    <p>5</p>
+    <p>6</p>
+    <p>7</p>
+    <p>8</p>
+    <p>9</p>
+    <p>10</p>
+    <p>11</p>
+    <p>12</p>
+    <p>13</p>
+    <p>14</p>
+    <p>15</p>
+    <p>16</p>
+    <p>17</p>
+    <p>18</p>
+    <p>19</p>
+    <p>20</p>
+    <p>21</p>
+    <p>22</p>
+    <p>23</p>
+    <p>24</p>
+    <p>25</p>
+    <p>26</p>
+    <p>27</p>
+    <p>28</p>
+    <p>29</p>
+    <p>30</p>
+    <p>31</p>
+    <p>32</p>
+    <p>33</p>
+    <p>34</p>
+    <p>35</p>
+    <p>36</p>
+    <p>37</p>
+    <p>38</p>
+    <p>39</p>
+    <p>40</p>
+    <p>41</p>
+    <p>42</p>
+    <p>43</p>
+    <p>44</p>
+    <p>45</p>
+    <p>46</p>
+    <p>47</p>
+    <p>48</p>
+    <p>49</p>
+    <p>50</p>
   </div>
 </template>
 
 <script>
   import NavBar from "components/common/navbar/NavBar";
   import HomeSwiper from "./childComps/HomeSwiper";
-  import {getHomeMultidata} from "network/home";
   import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+  import TabControl from "../../components/content/tabcontrol/TabControl";
 
+  import {getHomeMultidata} from "network/home";
   export default {
     name: "Home",
     components: {
       NavBar,
       HomeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView,
+      TabControl
     },
     data() {
       return {
@@ -43,8 +99,20 @@
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+  }
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 2;
+  }
+  .tab-control{
+    position: sticky;
+    top: 44px;
   }
 </style>
